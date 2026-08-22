f76efa038ae1731cf0d1b23bca674a7b9a54b0f4
zhengtong27 <2948006186@qq.com>
2026-08-22 23:35:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request, jsonify, render_template, Response
+from flask import Flask, request, jsonify, render_template, Respons
 import re
 import os
 import json
